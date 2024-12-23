d1bfc0ebe6338dd13e14b0a2e3757fc3c2f75a30
Reveal1022 <168961935+Reveal1022@users.noreply.github.com>
2024-12-23 16:06:02 +0545

    Css task added

---
+++ src/app/cssTask/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./task.css";
+
+const page = () => {
+  return (
+    <div>
+      <div className="div1">Widget 1</div>
+      <div className="div2">Widget 2</div>
+      <div className="div3">Widget 3</div>
+      <div className="div4">Widget 4</div>
+      <div className="div5">Widget 5</div>
+      <div className="div6">Widget 6</div>
+    </div>
+  );
+};
+
+export default page;
